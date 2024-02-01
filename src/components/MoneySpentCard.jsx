eb4b1123598ad5c1aaf83104916976474e8d4362
Albin Antony <albinantonydominic@gmail.com>
2024-02-01 16:44:57 +0530

    symbol

--- src/components/MoneySpentCard.jsx
+++ src/components/MoneySpentCard.jsx
@@ -12,7 +12,7 @@ const totalAmount = groupedExpenses.reduce((acc,curr)=>acc+ curr.amount,0)
     style={{boxShadow:" rgba(0, 0, 0, 0.09) 0px 3px 12px"}} 
     className=' border max-w-[400px] p-3 flex flex-grow  flex-col items-start gap-4  rounded-lg  '>
       <h1 className='font-bold text-2xl'>Total Spent</h1>
-      <h1 className='font-bold text-2xl w-full text-right'>{totalAmount.toLocaleString()}</h1>
+      <h1 className='font-bold text-2xl w-full text-right'>&#8377; {totalAmount.toLocaleString()}</h1>
 
     </div>
   )
